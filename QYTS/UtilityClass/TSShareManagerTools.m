1c895e4131a3eab0ad4bdf8a96a835b0a27f5cc0
lxd <75509218@qq.com>
2017-10-16 18:35:22 +0800

    bcbc1

--- QYTS/UtilityClass/TSShareManagerTools.m
+++ QYTS/UtilityClass/TSShareManagerTools.m
@@ -9,7 +9,7 @@
 #import "TSShareManagerTools.h"
 #import "PersonalViewModel.h"
 
-static NSString *WebPageUrl = @"http://test2.qiuyouzone.com/statis_share/videoshare.html?matchId=%@";
+static NSString *WebPageUrl = @"http://bcbc1.qiuyouzone.com/statis_share/videoshare.html?matchId=%@";
 static NSString *ThumbURL = @"";
 static NSString *kLinkTitle = @"语音技统";
 static NSString *kLinkDescription = @"#战在BCBC#灯光与球场，汗水与信念，今天我是球场王者！";
@@ -78,7 +78,7 @@ static NSString *kLinkDescription = @"#战在BCBC#灯光与球场，汗水与信
             } else if (currentUserType == LoginUserTypeCBO) {
                 paramsDict[@"type"] = @3;
             }
-            paramsDict[@"url"] = @"http://test2.qiuyouzone.com/statis_share/videoshare.html";
+            paramsDict[@"url"] = @"http://bcbc1.qiuyouzone.com/statis_share/videoshare.html";
             PersonalViewModel *personalViewModel = [[PersonalViewModel alloc] initWithPramasDict:paramsDict];
             [personalViewModel setBlockWithReturnBlock:^(id returnValue) {
                 DDLog(@"shareLogSave returnValue is:%@", returnValue);
